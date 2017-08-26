bd588836e248842a793d7a54af3d00ce1f229c01
Magnus Havgry <magnushavgry@gmail.com>
2017-08-26 12:11:29 +0200

    Add Google Analytics tracking

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { sync } from 'vuex-router-sync'
+import VueAnalytics from 'vue-analytics'
 
 import App from './components/App.vue'
 import router from './router'
 import store from './store'
 
 Vue.use(VueRouter)
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-27432261-5',
+    router,
+  })
+}
 
 sync(store, router)
 
