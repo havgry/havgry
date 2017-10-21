301e0ad09c9c7ce4882b91b9b1f798dddaca9bd4
Magnus Havgry <magnushavgry@gmail.com>
2017-10-21 13:56:45 +0200

    Clean webpack config (I don't like red lines)

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-var path = require('path')
-var webpack = require('webpack')
-var WebpackNotifierPlugin = require('webpack-notifier')
+const path = require('path')
+const webpack = require('webpack')
+const WebpackNotifierPlugin = require('webpack-notifier')
+const cssnext = require('postcss-cssnext')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './assets'),
     publicPath: '/assets/',
-    filename: 'build.js'
+    filename: 'build.js',
   },
   resolve: {
     modules: ['src', 'node_modules'],
@@ -20,37 +21,37 @@ module.exports = {
         enforce: 'pre',
         test: /\.vue$/,
         loader: 'eslint-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       // But use vue-loader for all *.vue files
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          postcss: [require('postcss-cssnext')()]
-        }
+          postcss: [cssnext],
+        },
       },
       {
         test: /\.js$/,
         loader: ['babel-loader', 'eslint-loader'],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         query: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
+          name: '[name].[ext]?[hash]',
+        },
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true
+    noInfo: true,
   },
   devtool: '#eval-source-map',
   plugins: [
-    new WebpackNotifierPlugin,
+    new WebpackNotifierPlugin(),
   ],
 }
 
@@ -60,8 +61,8 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
-      }
+        NODE_ENV: '"production"',
+      },
     }),
   ])
 }
