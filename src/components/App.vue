cf1e2d1dc80f8f6082e7ac54ef4526b4d860778d
Magnus Havgry <magnushavgry@gmail.com>
2017-11-18 23:45:42 +0100

    Move pages to separate file

--- src/components/App.vue
+++ src/components/App.vue
@@ -33,38 +33,13 @@
 
 <script>
 import { mapState } from 'vuex'
+import pages from '../content/pages'
 
 export default {
   name: 'app',
   data: () => ({
+    pages,
     isHoveringLink: false,
-    pages: [
-      {
-        name: 'ideas',
-        prefix: null,
-        namePrefix: 'I think',
-        paragraphs: [
-          'I\'m particularly fond of ideas with potential to make a real impact on people\'s lives and the environment.',
-        ],
-      },
-      {
-        name: 'prototypes',
-        prefix: ', ',
-        namePrefix: 'I design',
-        paragraphs: [
-          'I find the creative process of turning ideas and concepts into working prototypes to be challenging and deeply satisfying.',
-        ],
-      },
-      {
-        name: 'code',
-        prefix: ', and ',
-        namePrefix: 'I write',
-        paragraphs: [
-          'I do frontend development, and I\'m currently pretty excited about Vue and GraphQL.',
-          '<a href="https://github.com/havgry">This thingie takes you to my GitHub profile</a>.',
-        ],
-      },
-    ],
   }),
   computed: {
     ...mapState({
---
+++ src/content/pages.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    name: 'ideas',
+    prefix: null,
+    namePrefix: 'I think',
+    paragraphs: [
+      'I\'m particularly fond of ideas with potential to make a real impact on people\'s lives and the environment.',
+    ],
+  },
+  {
+    name: 'prototypes',
+    prefix: ', ',
+    namePrefix: 'I design',
+    paragraphs: [
+      'I find the creative process of turning ideas and concepts into working prototypes to be deeply satisfying.',
+    ],
+  },
+  {
+    name: 'code',
+    prefix: ', and ',
+    namePrefix: 'I write',
+    paragraphs: [
+      'I do frontend development, and I\'m currently pretty excited about Vue and GraphQL.',
+      '<a href="https://github.com/havgry">This thingie takes you to my GitHub profile</a>.',
+    ],
+  },
+]
